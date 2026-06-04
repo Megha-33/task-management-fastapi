7871f8c0df2a8df8239c8eb61d1732ccc7dab316
Megha-33 <vobugarisaimeghana@gmail.com>
2026-06-04 12:08:56 +0530

    feat: add docker support

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Running database migrations..."
+
+alembic upgrade head
+
+echo "Starting FastAPI application..."
+
+uvicorn app.main:app \
+    --host 0.0.0.0 \
+    --port 8000
